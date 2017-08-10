e18035acaf424a83081d2947cccff785f89fe024
Zoe Lee <zoe@kihira.uk>
2017-08-10 16:28:18 +0100

    Add support for sending crash report to the server

--- src/main/java/uk/kihira/crashcallback/CrashCallback.java
+++ src/main/java/uk/kihira/crashcallback/CrashCallback.java
@@ -11,21 +11,24 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "crashcallback", name = "CrashCallback", acceptedMinecraftVersions = "[1,)")
 public class CrashCallback {
-    private static final Logger logger = LogManager.getLogger("CrashCallback");
+    public static final Logger logger = LogManager.getLogger("CrashCallback");
     public static String callbackUrl;
     public static int expectedResponse;
+    public static boolean sendCrash;
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent e) {
         // Load configuration data first
         Configuration config = new Configuration(e.getSuggestedConfigurationFile());
-        callbackUrl = config.getString("Callback", Configuration.CATEGORY_GENERAL, "", "The URL that is GET on crash");
+        callbackUrl = config.getString("Callback", Configuration.CATEGORY_GENERAL, "", "The URL that is called on crash");
         expectedResponse = config.getInt("Response Code", Configuration.CATEGORY_GENERAL, 200, 0, 1000, "Expected HTTP response code from callback");
+        sendCrash = config.getBoolean("Send Crash Report", Configuration.CATEGORY_GENERAL, false, "Whether to send the crash report to the server as a POST");
         config.save();
 
         // Load from command line (overrides config options)
         callbackUrl = System.getProperty("cc.callback", callbackUrl);
         expectedResponse = Integer.parseInt(System.getProperty("cc.response", String.valueOf(expectedResponse)));
+        sendCrash = Boolean.parseBoolean(System.getProperty("cc.sendCrash", String.valueOf(sendCrash)));
 
         if (StringUtils.isNullOrEmpty(callbackUrl)) {
             logger.error("No callback URL is set!");
@@ -33,5 +36,10 @@ public class CrashCallback {
         }
 
         FMLCommonHandler.instance().registerCrashCallable(new CrashHandler());
+    }
+
+    @Mod.EventHandler
+    public void onServerStart(FMLServerStartedEvent e) {
+        throw new RuntimeException("Oh noes!");
     }
 }
--- src/main/java/uk/kihira/crashcallback/CrashHandler.java
+++ src/main/java/uk/kihira/crashcallback/CrashHandler.java
@@ -1,9 +1,17 @@
 package uk.kihira.crashcallback;
 
+import joptsimple.internal.Strings;
 import net.minecraftforge.fml.common.ICrashCallable;
 
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class CrashHandler implements ICrashCallable {
     @Override
@@ -13,17 +21,52 @@ public class CrashHandler implements ICrashCallable {
 
     @Override
     public String call() throws Exception {
-        //RuntimeMXBean factory = ManagementFactory.getRuntimeMXBean();
-        //List<String> args = factory.getInputArguments();
-
         URL url = new URL(CrashCallback.callbackUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        //conn.setDoOutput(true);
-        conn.setRequestMethod("GET");
-        conn.setConnectTimeout(5000);
+        conn.setConnectTimeout(2000);
+        conn.setReadTimeout(2000);
 
-        if (conn.getResponseCode() != CrashCallback.expectedResponse) {
-            return "Failed to call CrashCallback url\nGot bad response code: " + conn.getResponseCode();
+        if (CrashCallback.sendCrash) {
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    List<String> data = new ArrayList<>();
+                    Path path = Paths.get("./crash-reports/");
+                    Optional<Path> lastFile = Files.list(path)
+                            .filter(f -> !Files.isDirectory(f))
+                            .max((f1, f2) -> (int)(f1.toFile().lastModified() - f2.toFile().lastModified()));
+
+                    if (lastFile.isPresent()) {
+                        data =  Files.readAllLines(lastFile.get());
+                    }
+                    else {
+                        data.add("Failed to load crash report");
+                    }
+
+                    // todo: Spawn a different process instead? This might get killed too soon
+
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(Strings.join(data, "\n"));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+
+                    if (conn.getResponseCode() != CrashCallback.expectedResponse) {
+                        System.err.println("Failed to call CrashCallback url\nGot bad response code: " + conn.getResponseCode());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }));
+        }
+        else {
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() != CrashCallback.expectedResponse) {
+                return "Failed to call CrashCallback url\nGot bad response code: " + conn.getResponseCode();
+            }
         }
 
         return "";
